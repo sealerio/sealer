f746ee677e0c0be38909ed9adf7fe5f376565647
cuisongliu <cuisongliu@qq.com>
2021-05-20 22:43:10 +0800

    feat(main): fix metadata not found

--- runtime/init.go
+++ runtime/init.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/alibaba/sealer/image"
+	"github.com/alibaba/sealer/common"
 
 	"github.com/alibaba/sealer/logger"
 
@@ -34,7 +34,16 @@ func (d *Default) init(cluster *v1.Cluster) error {
 			return err
 		}
 	*/
-
+	//metadata
+	clusterTmpRootfsMetadata := filepath.Join("/tmp", cluster.Name, common.DefaultMetadataName)
+	if err := utils.MkDirIfNotExists(d.Rootfs); err != nil {
+		return err
+	}
+	metadataPath := fmt.Sprintf("%s/%s", d.Rootfs, common.DefaultMetadataName)
+	if err := utils.Cmd("cp", clusterTmpRootfsMetadata, metadataPath); err != nil {
+		return err
+	}
+	d.LoadMetadata()
 	//config kubeadm
 	if err := d.ConfigKubeadmOnMaster0(); err != nil {
 		return err
@@ -83,8 +92,7 @@ func (d *Default) initRunner(cluster *v1.Cluster) error {
 	d.StaticFileDir = fmt.Sprintf("%s/statics", d.Rootfs)
 	// TODO remote port in ipList
 	d.APIServerCertSANs = append(cluster.Spec.CertSANS, d.getDefaultSANs()...)
-	metadataFile := image.GetMetadataFromImage(cluster.Spec.Image)
-	d.Metadata = NewMetadata(metadataFile)
+	d.LoadMetadata()
 	d.Interface = cluster.Spec.Network.Interface
 	d.Network = cluster.Spec.Network.CNIName
 	d.PodCIDR = cluster.Spec.Network.PodCIDR
