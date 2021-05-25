6055e5c0e3aa734579ebc3c5db301a680b077561
maybaby <berlinsaint@126.com>
2021-05-25 22:24:23 +0800

    PR for sealer run support podcidr #169 translate eng to cn

--- utils/cidr.go
+++ utils/cidr.go
@@ -7,16 +7,16 @@ import (
 
 /*
 	https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
-	CIDR表示法:
-	IPv4   	网络号/前缀长度		192.168.1.0/24
-	IPv6	接口号/前缀长度		2001:db8::/64
+	CIDR doc:
+	IPv4   	network Addr/prefixLength		192.168.1.0/24
+	IPv6	network Addr/prefixLength		2001:db8::/64
 */
 type CIDR struct {
 	ip    net.IP
 	ipnet *net.IPNet
 }
 
-// 解析CIDR网段
+// Parse CIDR net range
 func ParseCIDR(s string) (*CIDR, error) {
 	i, n, err := net.ParseCIDR(s)
 	if err != nil {
@@ -25,7 +25,7 @@ func ParseCIDR(s string) (*CIDR, error) {
 	return &CIDR{ip: i, ipnet: n}, nil
 }
 
-// 解析并校准CIDR
+// Parse CIDR  Fix to Standard CIDR
 func ParseCIDRString(s string) (string, error) {
 	c, err := ParseCIDR(s)
 	if err != nil {
@@ -34,41 +34,41 @@ func ParseCIDRString(s string) (string, error) {
 	return c.CIDR(), nil
 }
 
-// 判断是否IPv4
+// Is IPv4
 func (c CIDR) IsIPv4() bool {
 	_, bits := c.ipnet.Mask.Size()
 	return bits/8 == net.IPv4len
 }
 
-// 判断是否IPv6
+// Is IPv6
 func (c CIDR) IsIPv6() bool {
 	_, bits := c.ipnet.Mask.Size()
 	return bits/8 == net.IPv6len
 }
 
-// CIDR字符串中的IP部分
+// Get IP
 func (c CIDR) IP() string {
 	return c.ip.String()
 }
 
-// 网络号
+// Get Network Addr
 func (c CIDR) Network() string {
 	return c.ipnet.IP.String()
 }
 
-// 子网掩码位数
+// Get Mask Size
 func (c CIDR) MaskSize() (ones, bits int) {
 	ones, bits = c.ipnet.Mask.Size()
 	return
 }
 
-// 子网掩码
+// SubnetMask
 func (c CIDR) Mask() string {
 	mask, _ := hex.DecodeString(c.ipnet.Mask.String())
 	return net.IP([]byte(mask)).String()
 }
 
-// 根据子网掩码长度校准后的CIDR
+// Fixed CIDR String
 func (c CIDR) CIDR() string {
 	return c.ipnet.String()
 }
