560eba0de0959f8c54b16a02491961b8b98ce86b
Eric Bao <baojn@zju.edu.cn>
2021-06-07 09:59:36 +0800

    use docker registry api to take over the communication with registry (#236)

--- image/distributionutil/config.go
+++ image/distributionutil/config.go
@@ -15,13 +15,24 @@
 package distributionutil
 
 import (
+	"time"
+
+	"github.com/alibaba/sealer/image/reference"
 	"github.com/alibaba/sealer/image/store"
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/pkg/progress"
 )
 
 type Config struct {
 	LayerStore     store.LayerStore
 	ProgressOutput progress.Output
-	AuthInfo       types.AuthConfig
+	Named          reference.Named
+}
+
+type registryConfig struct {
+	Domain   string
+	Insecure bool
+	SkipPing bool
+	NonSSL   bool
+	Timeout  time.Duration
+	Headers  map[string]string
 }
