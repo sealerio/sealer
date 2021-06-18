e0cec43b8d0ab6c653d22e92a2177e0215833ff7
zhangshu <2542652@qq.com>
2021-06-18 12:04:42 +0800

    feature: add twice interacition when sealer delete
    when use sealer delete . Its a dangerous comand. it should be twice interacition with user.

--- sealer/cmd/delete.go
+++ sealer/cmd/delete.go
@@ -15,16 +15,13 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/alibaba/sealer/apply"
 	"github.com/alibaba/sealer/logger"
 	"github.com/spf13/cobra"
-)
-
-const (
-	yes = "y"
-	no  = "n"
 )
 
 // deleteCmd represents the delete command
@@ -32,20 +29,29 @@ var deleteCmd = &cobra.Command{
 	Use:     "delete",
 	Short:   "delete a cluster",
 	Long:    `if provider is BARESERVER will delete kubernetes nodes, or if provider is ALI_CLOUD, will delete all the infra resources`,
-	Example: `sealer delete -f /root/.sealer/mycluster/Clusterfile`,
+	Example: `sealer delete -f /root/.sealer/mycluster/Clusterfile [--force]`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var input string
-		for {
-			fmt.Printf("Are you sure you want to delete the cluster? Yes [Y/y], No [N/n] : ")
-			fmt.Scanln(&input)
-			if strings.EqualFold(yes, input) {
-				break
+		force, err := cmd.Flags().GetBool("force")
+		if err != nil {
+			logger.Error(err)
+			os.Exit(1)
+		}
+		if !force {
+			var yesRx = regexp.MustCompile("^(?:y(?:es)?)$")
+			var noRx = regexp.MustCompile("^(?:n(?:o)?)$")
+			var input string
+			for {
+				fmt.Printf("Are you sure you want to delete the cluster? Yes [y/yes], No [n/no] : ")
+				fmt.Scanln(&input)
+				if yesRx.MatchString(input) {
+					break
+				}
+				if noRx.MatchString(input) {
+					fmt.Println("You have canceled to delete the cluster!")
+					os.Exit(1)
+				}
 			}
-			if strings.EqualFold(no, input) {
-				fmt.Println("You have canceled to delete the cluster!")
-				os.Exit(1)
-			}
-		}		
+		}
 		if err := apply.NewApplierFromFile(clusterFile).Delete(); err != nil {
 			logger.Error(err)
 			os.Exit(1)
@@ -56,4 +62,5 @@ var deleteCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(deleteCmd)
 	deleteCmd.Flags().StringVarP(&clusterFile, "Clusterfile", "f", "Clusterfile", "delete a kubernetes cluster with Clusterfile Annotations")
+	deleteCmd.Flags().BoolP("force", "", false, "We also can input an --force flag to delete cluster by force")
 }
