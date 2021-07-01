f5446fda83c36356b22a3633af44e85096f153fc
lllwan <lllwan@vip.qq.com>
2021-07-01 19:32:03 +0800

    Support custom registry volume and add data validation

--- runtime/registry.go
+++ runtime/registry.go
@@ -25,35 +25,44 @@ import (
 	"github.com/alibaba/sealer/common"
 )
 
-func (d *Default) getRegistryHost() (host string) {
-	ip, domain := d.getRegistryConfig()
-	return fmt.Sprintf("%s %s", ip, domain)
+type RegistryConfig struct {
+	IP     string
+	Domain string
+	Volume string
 }
 
-func (d *Default) getRegistryConfig() (host, domain string) {
-	var config map[string]string
+func (d *Default) getRegistryHost() (host string) {
+	cf := d.getRegistryConfig()
+	if cf == nil {
+		return fmt.Sprintf("%s %s", d.Masters[0], SeaHub)
+	}
+	return fmt.Sprintf("%s %s", cf.IP, cf.Domain)
+}
+
+func (d *Default) getRegistryConfig() *RegistryConfig {
+	var config RegistryConfig
 	registryConfigPath := filepath.Join(common.DefaultClusterBaseDir(d.ClusterName), "etc/registry.yaml")
 	if utils.IsFileExist(registryConfigPath) {
 		err := utils.UnmarshalYamlFile(registryConfigPath, &config)
-		if err == nil {
-			return config["ip"], config["domain"]
+		if err == nil && config.IP != "" && config.Domain != "" && config.Volume != "" {
+			return &config
 		}
 		logger.Error("Failed to read registry config! ")
 	}
-	return d.Masters[0], SeaHub
+	return nil
 }
 
 const registryName = "sealer-registry"
 
 //Only use this for join and init, due to the initiation operations
 func (d *Default) EnsureRegistry() error {
-	ip, _ := d.getRegistryConfig()
-	cmd := fmt.Sprintf("cd %s/scripts && sh init-registry.sh 5000 %s/registry", d.Rootfs, d.Rootfs)
-	return d.SSH.CmdAsync(ip, cmd)
+	cf := d.getRegistryConfig()
+	cmd := fmt.Sprintf("cd %s/scripts && sh init-registry.sh 5000 %s/registry %s", d.Rootfs, d.Rootfs, cf.Volume)
+	return d.SSH.CmdAsync(cf.IP, cmd)
 }
 
 func (d *Default) RecycleRegistry() error {
-	ip, _ := d.getRegistryConfig()
+	cf := d.getRegistryConfig()
 	cmd := fmt.Sprintf("docker stop %s || true && docker rm %s || true", registryName, registryName)
-	return d.SSH.CmdAsync(ip, cmd)
+	return d.SSH.CmdAsync(cf.IP, cmd)
 }
