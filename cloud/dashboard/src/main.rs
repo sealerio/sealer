26b1a37a666a42ce84b682480239f9411e99ab13
中弈 <zhongyi.fht@alibaba-inc.com>
2021-07-19 15:49:20 +0800

    complete header

--- cloud/dashboard/src/main.rs
+++ cloud/dashboard/src/main.rs
@@ -1,7 +1,10 @@
+pub mod components;
+
 use yew::prelude::*;
+use crate::components::{header::Header};
+
 
 enum Msg {
-    AddOne,
 }
 
 struct Model {
@@ -23,38 +26,16 @@ impl Component for Model {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
         html! {
-            <section class="section">
-                <div class="container">
-                <h1 class="title">
-                    { "hello world" }
-                </h1>
-                <a class="button is-primary">
-                  { "Button" }
-                </a>
-                <p class="subtitle">
-                    { "My first website" } 
-                </p>
-                </div>
-            </section>
+            <Header />
         }
     }
 }
