baa5e29af48b432e4950529501155acfb0091ab5
cuisongliu <cuisongliu@qq.com>
2021-07-21 09:17:47 +0800

    fix(main): fix rootfs struct spilt clean shell

---
+++ filesystem/rootfs/rootfs/scripts/clean-kube.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -f /usr/bin/conntrack
+rm -f /usr/bin/kubelet-pre-start.sh
+rm -f /usr/bin/crictl
+rm -f /usr/bin/kubeadm
+rm -f /usr/bin/kubetcl
+rm -f /usr/bin/kubelet
+rm -f /usr/bin/containerd-rootless-setuptool.sh
+rm -f /usr/bin/containerd-rootless.sh
+rm -f /usr/bin/nerdctl
+
+rm -f /etc/sysctl.d/k8s.conf
+rm -f /etc/systemd/system/kubelet.service
+rm -rf /etc/systemd/system/kubelet.service.d
+rm -rf /var/lib/kubelet/
+rm -f /var/lib/kubelet/config.yaml
