d24dd4f783555bf6499d89f0ec0069726790994c
jinnzy <839444083@qq.com>
2021-07-26 10:07:12 +0800

    add plugin interface

---
+++ plugin/plugin.go
@@ -0,0 +1,22 @@
+package plugin
+
+import (
+	v1 "github.com/alibaba/sealer/types/api/v1"
+)
+
+type Interface interface {
+	Run(context Context, phase Phase)
+}
+
+type Phase string
+
+const (
+	PhasePreInit     = Phase("PreInit")
+	PhasePreInstall  = Phase("PreInstall")
+	PhasePostInstall = Phase("PostInstall")
+)
+
+type Context struct {
+	Cluster *v1.Cluster
+	Plugin  *v1.Plugin
+}
