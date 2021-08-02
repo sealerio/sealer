6652bad98a38020a72150ef5f0ca29c49a913abd
Byboycn <admin@0win.cn>
2021-08-02 21:04:46 +0800

    add pluginConfig file

---
+++ plugin/pluginConfig_test.go
@@ -0,0 +1,16 @@
+package plugin
+
+import (
+	"testing"
+
+	v1 "github.com/alibaba/sealer/types/api/v1"
+)
+
+func TestDumper_Dump(t *testing.T) {
+
+	type fields struct {
+		configs     []v1.Plugin
+		clusterName string
+	}
+
+}
