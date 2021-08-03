3a6f0d1d6f355398b9754d1e0367b2d3f19c4538
w8589167 <445839655@qq.com>
2021-08-03 09:15:29 +0800

    implement the shell plugin

--- plugin/shell_plugin.go
+++ plugin/shell_plugin.go
@@ -1,13 +1,22 @@
 package plugin
 
 import (
+	"github.com/alibaba/sealer/types/api/v1"
 	"github.com/alibaba/sealer/utils"
 )
 
 type Sheller struct {
-	role string
-	data string
 }
-func (s Sheller) Run(context Context, phase Phase){
-	s.SSH.CmdAsync()
+
+func (s Sheller) Run(context Context, phase Phase) {
+	var ps v1.PluginSpec
+	cmds := ps.Data
+
+	var hs v1.Hosts
+	host := hs.IPList
+
+	ssh := NewSSHByCluster(context.Cluster)
+	for i:= 0, i< len(host), i++ {
+		ssh.CmdAsync(host[i], cmds)
+	}
 }
