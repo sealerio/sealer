b386c4445f353e404426cc84634e2ce8b491e338
liutao841204 <liutao_free@126.com>
2021-08-08 15:38:52 +0800

    Refactoring Method
    Signed-off-by: liutao841204 <liutao_free@126.com>

--- plugin/etcd_backup_plugin.go
+++ plugin/etcd_backup_plugin.go
@@ -4,9 +4,9 @@ import (
 	ctx "context"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -22,43 +22,66 @@ type EtcdBackupPlugin struct {
 	backDir string
 }
 
-func (e EtcdBackupPlugin) Run(context Context, phase Phase) {
-	dialTimeout := 5 * time.Second
-	masterIP := context.Cluster.Spec.Masters.IPList[0]
-	if masterIP == "" {
-		logger.Error("Cluster does not exist \n")
-		os.Exit(1)
+func (e EtcdBackupPlugin) Run(context Context, phase Phase) error {
+	masterIP, err := getMasterIp(context)
+	if err != nil {
+		return err
 	}
-	endpoints := []string{fmt.Sprintf("https://%s:2379", masterIP)}
+
+	if err := fetchRemoteCert(context, masterIP); err != nil {
+		return err
+	}
+
+	cfg, err := connEtcd(masterIP)
+	if err != nil {
+		return err
+	}
+
+	return snapshotEtcd(&e, cfg)
+}
+
+func getMasterIp(context Context) (string, error) {
+	ipList := context.Cluster.Spec.Masters.IPList
+	if len(ipList) == 0 {
+		return "", errors.New("cluster master does not exist \n")
+	} else {
+		return ipList[0], nil
+	}
+}
+
+func fetchRemoteCert(context Context, masterIP string) error {
 	SSH := ssh.NewSSHByCluster(context.Cluster)
 	if err := SSH.Fetch(masterIP, "/tmp/healthcheck-client.crt", "/etc/kubernetes/pki/etcd/healthcheck-client.crt"); err != nil {
 		logger.Error("host %s healthcheck-client.crt file does not exist, err: %v\n", masterIP, err)
-		os.Exit(1)
+		return err
 	}
 	if err := SSH.Fetch(masterIP, "/tmp/healthcheck-client.key", "/etc/kubernetes/pki/etcd/healthcheck-client.key"); err != nil {
 		logger.Error("host %s healthcheck-client.crt file does not exist, err: %v\n", masterIP, err)
-		os.Exit(1)
+		return err
 	}
 	if err := SSH.Fetch(masterIP, "/tmp/ca.crt", "/etc/kubernetes/pki/etcd/ca.crt"); err != nil {
 		logger.Error("host %s healthcheck-client.crt file does not exist, err: %v\n", masterIP, err)
-		os.Exit(1)
+		return err
 	}
+	return nil
+}
 
-	etcdCert := "/tmp/healthcheck-client.crt"
-	etcdCertKey := "/tmp/healthcheck-client.key"
-	etcdCa := "/tmp/ca.crt"
+func connEtcd(masterIP string) (clientv3.Config, error) {
+	const dialTimeout = 5 * time.Second
+	const etcdCert = "/tmp/healthcheck-client.crt"
+	const etcdCertKey = "/tmp/healthcheck-client.key"
+	const etcdCa = "/tmp/ca.crt"
 
-	// 创建连接-TLS
 	cert, err := tls.LoadX509KeyPair(etcdCert, etcdCertKey)
 	if err != nil {
 		logger.Error("cacert or key file is not exist, err:%v\n", err)
-		os.Exit(1)
+		return clientv3.Config{}, err
 	}
 
 	caData, err := ioutil.ReadFile(etcdCa)
 	if err != nil {
 		logger.Error("ca certificate reading failed, err:%v\n", err)
-		os.Exit(1)
+		return clientv3.Config{}, err
 	}
 
 	pool := x509.NewCertPool()
@@ -69,6 +92,7 @@ func (e EtcdBackupPlugin) Run(context Context, phase Phase) {
 		RootCAs:      pool,
 	}
 
+	endpoints := []string{fmt.Sprintf("https://%s:2379", masterIP)}
 	cfg := clientv3.Config{
 		Endpoints:   endpoints,
 		DialTimeout: dialTimeout,
@@ -78,17 +102,21 @@ func (e EtcdBackupPlugin) Run(context Context, phase Phase) {
 	cli, err := clientv3.New(cfg)
 	if err != nil {
 		logger.Error("connect to etcd failed, err:%v\n", err)
-		os.Exit(1)
+		return clientv3.Config{}, err
 	}
 
 	fmt.Println("connect to etcd success")
 
 	defer cli.Close()
 
+	return cfg, nil
+}
+
+func snapshotEtcd(e *EtcdBackupPlugin, cfg clientv3.Config) error {
 	lg, err := zap.NewProduction()
 	if err != nil {
 		logger.Error("get lg error, err:%v\n", err)
-		os.Exit(1)
+		return err
 	}
 
 	ctx, cancel := ctx.WithCancel(ctx.Background())
@@ -97,7 +125,9 @@ func (e EtcdBackupPlugin) Run(context Context, phase Phase) {
 	var dbPath = fmt.Sprintf("%s/%s", e.backDir, e.name)
 	if err := snapshot.Save(ctx, lg, cfg, dbPath); err != nil {
 		logger.Error("snapshot save err: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 	logger.Info("Snapshot saved at %s\n", dbPath)
+
+	return nil
 }
