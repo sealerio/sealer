67a19036ec517d606889f61655cbe525be451f7c
liutao841204 <liutao_free@126.com>
2021-08-10 21:54:37 +0800

    fix unusedresult: result of fmt.Errorf call not used (govet)

--- plugin/etcd_backup_plugin.go
+++ plugin/etcd_backup_plugin.go
@@ -56,8 +56,7 @@ func fetchRemoteCert(context Context, masterIP string) error {
 	certs := []string{"healthcheck-client.crt", "healthcheck-client.key", "ca.crt"}
 	for _, cert := range certs {
 		if err := SSH.Fetch(masterIP, "/tmp/"+cert, "/etc/kubernetes/pki/etcd/"+cert); err != nil {
-			fmt.Errorf("host %s %s file does not exist, err: %v", masterIP, cert, err)
-			return err
+			return fmt.Errorf("host %s %s file does not exist, err: %v", masterIP, cert, err)
 		}
 	}
 	return nil
@@ -71,14 +70,12 @@ func connEtcd(masterIP string) (clientv3.Config, error) {
 
 	cert, err := tls.LoadX509KeyPair(etcdCert, etcdCertKey)
 	if err != nil {
-		fmt.Errorf("cacert or key file is not exist, err:%v", err)
-		return clientv3.Config{}, err
+		return clientv3.Config{}, fmt.Errorf("cacert or key file is not exist, err:%v", err)
 	}
 
 	caData, err := ioutil.ReadFile(etcdCa)
 	if err != nil {
-		fmt.Errorf("ca certificate reading failed, err:%v", err)
-		return clientv3.Config{}, err
+		return clientv3.Config{}, fmt.Errorf("ca certificate reading failed, err:%v", err)
 	}
 
 	pool := x509.NewCertPool()
@@ -98,8 +95,7 @@ func connEtcd(masterIP string) (clientv3.Config, error) {
 
 	cli, err := clientv3.New(cfg)
 	if err != nil {
-		fmt.Errorf("connect to etcd failed, err:%v", err)
-		return clientv3.Config{}, err
+		return clientv3.Config{}, fmt.Errorf("connect to etcd failed, err:%v", err)
 	}
 
 	fmt.Printf("connect to etcd success")
@@ -112,8 +108,7 @@ func connEtcd(masterIP string) (clientv3.Config, error) {
 func snapshotEtcd(e *EtcdBackupPlugin, cfg clientv3.Config) error {
 	lg, err := zap.NewProduction()
 	if err != nil {
-		fmt.Errorf("get lg error, err:%v", err)
-		return err
+		return fmt.Errorf("get lg error, err:%v", err)
 	}
 
 	ctx, cancel := ctx.WithCancel(ctx.Background())
@@ -121,10 +116,9 @@ func snapshotEtcd(e *EtcdBackupPlugin, cfg clientv3.Config) error {
 
 	var dbPath = fmt.Sprintf("%s/%s", e.backDir, e.name)
 	if err := snapshot.Save(ctx, lg, cfg, dbPath); err != nil {
-		fmt.Errorf("snapshot save err: %v", err)
-		return err
+		return fmt.Errorf("snapshot save err: %v", err)
 	}
-	fmt.Printf("Snapshot saved at %s", dbPath)
+	fmt.Printf("Snapshot saved at %v", dbPath)
 
 	return nil
 }
