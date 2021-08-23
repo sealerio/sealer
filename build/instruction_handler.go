0171efce289a0746a900b826f19edf880e2baf84
Jiahui <wb-hjh933779@alibaba-inc.com>
2021-08-23 11:07:44 +0800

    Release v0.3.3 (#520)
    * fix failed to write config files config is nil

--- build/instruction_handler.go
+++ build/instruction_handler.go
@@ -187,6 +187,10 @@ func (h *handler) copyFiles(srcFileName, dstFileName, tempBuildDir string) error
 	}
 
 	if fi.IsDir() {
+		//default workdir is rootfs,so if copy dst is ".", name it as filepath.Base(srcFileName)
+		if dstFileName == "." {
+			dstFileName = filepath.Base(srcFileName)
+		}
 		dst = filepath.Join(tempBuildDir, dstFileName)
 	} else {
 		dst = filepath.Join(tempBuildDir, dstFileName, srcFileName)
