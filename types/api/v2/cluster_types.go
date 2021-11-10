5982792e46a452d92b18ebc1a26e5e615c7e8e92
中弈 <zhongyi.fht@alibaba-inc.com>
2021-11-10 20:19:45 +0800

    add cluster v2

---
+++ types/api/v2/cluster_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2021 alibaba.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/alibaba/sealer/types/api/v1"
+)
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterSpec defines the desired state of Cluster
+type ClusterSpec struct {
+	// desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Cluster. Edit Cluster_types.go to remove/update
+	Image string            `json:"image,omitempty"`
+	Env   map[string]string `json:"env,omitempty"`
+	Hosts []Hosts           `json:"hosts,omitempty"`
+	SSH   v1.SSH            `json:"ssh,omitempty"`
+}
+
+type Hosts struct {
+	IP    string   `json:"ip,omitempty"`
+	Roles []string `json:"roles,omitempty"`
+	// SSH config overwrite
+	SSH v1.SSH `json:"ssh,omitempty"`
+	// env overwrite
+	Env map[string]string `json:"env,omitempty"`
+}
+
+// ClusterStatus defines the observed state of Cluster
+type ClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Cluster is the Schema for the clusters API
+type Cluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterSpec   `json:"spec,omitempty"`
+	Status ClusterStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterList contains a list of Cluster
+type ClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Cluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Cluster{}, &ClusterList{})
+}
