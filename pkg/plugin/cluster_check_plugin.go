9e2aa6a96d056a49a3432a89a17ea598af0603cc
wangfei <wf01031309@alibaba-inc.com>
2021-12-13 14:59:54 +0800

    Plugin module upgrade

---
+++ pkg/plugin/cluster_check_plugin.go
@@ -0,0 +1,91 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	goContext "context"
+	"fmt"
+	"time"
+
+	"github.com/alibaba/sealer/client/k8s"
+	"github.com/alibaba/sealer/logger"
+	"golang.org/x/net/context"
+)
+
+type ClusterChecker struct {
+	client *k8s.Client
+}
+
+func NewClusterCheckerPlugin() Interface {
+	return &ClusterChecker{}
+}
+
+func init() {
+	Register(ClusterCheckPlugin, &ClusterChecker{})
+}
+
+func (c *ClusterChecker) Run(context Context, phase Phase) error {
+	if phase != PhasePreGuest || context.Plugin.Spec.Type != ClusterCheckPlugin {
+		logger.Debug("check cluster is PreGuest!")
+		return nil
+	}
+	if err := c.waitClusterReady(goContext.TODO()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ClusterChecker) waitClusterReady(ctx goContext.Context) error {
+	var clusterStatusChan = make(chan string)
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Minute)
+	defer cancel()
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+	go func(t *time.Ticker) {
+		for {
+			clusterStatus := c.getClusterStatus()
+			clusterStatusChan <- clusterStatus
+			<-t.C
+		}
+	}(ticker)
+	for {
+		select {
+		case status := <-clusterStatusChan:
+			if status == ClusterNotReady {
+				logger.Info("wait for the cluster to ready ")
+			} else if status == ClusterReady {
+				logger.Info("cluster is ready now")
+				return nil
+			}
+		case <-ctx.Done():
+			return fmt.Errorf("cluster is not ready, please check")
+		}
+	}
+}
+
+func (c *ClusterChecker) getClusterStatus() string {
+	k8sClient, err := k8s.Newk8sClient()
+	c.client = k8sClient
+	if err != nil {
+		return ClusterNotReady
+	}
+
+	kubeSystemPodStatus, err := c.client.ListKubeSystemPodsStatus()
+	if !kubeSystemPodStatus || err != nil {
+		return ClusterNotReady
+	}
+
+	return ClusterReady
+}
---
+++ pkg/plugin/hostname_plugin.go
@@ -0,0 +1,103 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/alibaba/sealer/logger"
+	"github.com/alibaba/sealer/utils/ssh"
+)
+
+/*
+hostnames plugin in Clusterfile:
+---
+apiVersion: sealer.aliyun.com/v1alpha1
+kind: Plugin
+metadata:
+  name: HOSTNAME
+spec:
+  data: |
+     192.168.0.2 master-0
+     192.168.0.3 master-1
+     192.168.0.4 master-2
+     192.168.0.5 node-0
+     192.168.0.6 node-1
+     192.168.0.7 node-2
+---
+HostnamePlugin.data
+key = ip
+value = target hostname
+*/
+type HostnamePlugin struct {
+	data map[string]string
+}
+
+func NewHostnamePlugin() Interface {
+	return &HostnamePlugin{data: map[string]string{}}
+}
+
+func init() {
+	Register(HostNamePlugin, &HostnamePlugin{})
+}
+
+func (h HostnamePlugin) Run(context Context, phase Phase) error {
+	if phase != PhasePreInit || context.Plugin.Spec.Type != HostNamePlugin {
+		logger.Debug("hostnamePlugin nodes is not PhasePreInit!")
+		return nil
+	}
+	h.data = h.formatData(context.Plugin.Spec.Data)
+	SSH := ssh.NewSSHByClusterV2(context.Cluster)
+	for ip, hostname := range h.data {
+		err := h.changeNodeName(hostname, ip, SSH)
+		if err != nil {
+			return fmt.Errorf("current cluster nodes hostname change failed, %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (h HostnamePlugin) formatData(data string) map[string]string {
+	m := make(map[string]string)
+	items := strings.Split(data, "\n")
+	if len(items) == 0 {
+		logger.Debug("hostname data is empty!")
+		return m
+	}
+	for _, v := range items {
+		tmps := strings.Split(v, " ")
+		//skip no-compliance hostname data
+		if len(tmps) != 2 {
+			continue
+		}
+		ip := tmps[0]
+		hostname := tmps[1]
+		m[ip] = hostname
+	}
+	return m
+}
+
+func (h HostnamePlugin) changeNodeName(hostname, ip string, SSH ssh.Interface) error {
+	//cmd to change hostname temporarily
+	tmpCMD := fmt.Sprintf("hostname %s", hostname)
+	//cmd to change hostname permanently
+	perCMD := fmt.Sprintf(`rm -f /etc/hostname && echo "%s" >> /etc/hostname`, hostname)
+	if err := SSH.CmdAsync(ip, tmpCMD, perCMD); err != nil {
+		return fmt.Errorf("failed to change the node %v hostname,%v", ip, err)
+	}
+	return nil
+}
