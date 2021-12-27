fad31df5760a0488a4ed6baa101fbf084c4475f0
zhangshu <2542652@qq.com>
2021-12-27 11:13:55 +0800

    #952 feature: add sealer exec command (#956)

---
+++ pkg/exec/exec.go
@@ -0,0 +1,83 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exec
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/alibaba/sealer/common"
+	v2 "github.com/alibaba/sealer/types/api/v2"
+	"github.com/alibaba/sealer/utils"
+	"github.com/alibaba/sealer/utils/ssh"
+	"golang.org/x/sync/errgroup"
+)
+
+type Exec struct {
+	cluster *v2.Cluster
+	ipList  []string
+}
+
+func NewExecCmd(clusterName string, roles string) (Exec, error) {
+	if clusterName == "" {
+		var err error
+		clusterName, err = utils.GetDefaultClusterName()
+		if err != nil {
+			return Exec{}, err
+		}
+	}
+	clusterFile := common.GetClusterWorkClusterfile(clusterName)
+	cluster, err := utils.GetClusterFromFile(clusterFile)
+	if err != nil {
+		return Exec{}, err
+	}
+	var ipList []string
+	if roles == "" {
+		ipList = append(cluster.GetMasterIPList(), cluster.GetNodeIPList()...)
+	} else {
+		roles := strings.Split(roles, ",")
+		for _, role := range roles {
+			ipList = append(ipList, cluster.GetIPSByRole(role)...)
+		}
+		if len(ipList) == 0 {
+			return Exec{}, fmt.Errorf("failed to get ipList, please check your roles label")
+		}
+	}
+	return Exec{cluster: cluster, ipList: ipList}, nil
+}
+
+func (exec *Exec) RunCmd(args ...string) error {
+	eg, _ := errgroup.WithContext(context.Background())
+	ipList := exec.ipList
+	for _, ip := range ipList {
+		ip := ip
+		eg.Go(func() error {
+			sshClient, sshErr := ssh.GetHostSSHClient(ip, exec.cluster)
+			if sshErr != nil {
+				return sshErr
+			}
+			err := sshClient.CmdAsync(ip, args...)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return fmt.Errorf("failed to sealer exec command, err: %v", err)
+	}
+	return nil
+}
---
+++ sealer/cmd/exec.go
@@ -0,0 +1,50 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/alibaba/sealer/pkg/exec"
+	"github.com/spf13/cobra"
+)
+
+var roles string
+
+// execCmd represents the exec command
+var execCmd = &cobra.Command{
+	Use:   "exec",
+	Short: "exec a shell command or script on all node.",
+	Example: `
+exec to default cluster: my-cluster
+	sealer exec 'cat /etc/hosts'
+specify the cluster name(If there is only one cluster in the $HOME/.sealer directory, it should be applied. ):
+    sealer exec -c my-cluster 'cat /etc/hosts'
+set role label to exec cmd:
+    sealer exec -c my-cluster -r master,slave,node1 'cat /etc/hosts'		
+`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		execCmd, err := exec.NewExecCmd(clusterName, roles)
+		if err != nil {
+			return err
+		}
+		return execCmd.RunCmd(args...)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(execCmd)
+	execCmd.Flags().StringVarP(&clusterName, "cluster-name", "c", "", "submit one cluster name")
+	execCmd.Flags().StringVarP(&roles, "roles", "r", "", "set role label to roles")
+}
