1c5d2142cd5906e2b61af95f396c3256cc91a11e
Jiahui <wb-hjh933779@alibaba-inc.com>
2022-01-11 16:21:47 +0800

    move rootfs dir (#1073)

---
+++ pkg/filesystem/rootfs/docker/scripts/docker.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Copyright © 2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+get_distribution() {
+    lsb_dist=""
+    # Every system that we officially support has /etc/os-release
+    if [ -r /etc/os-release ]; then
+    	lsb_dist="$(. /etc/os-release && echo "$ID")"
+    fi
+    # Returning an empty string here should be alright since the
+    # case statements don't act unless you provide an actual value
+    echo "$lsb_dist"
+}
+disable_selinux(){
+    if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        setenforce 0
+    fi
+}
+set -x
+storage=${1:-/var/lib/docker}
+mkdir -p $storage
+if ! command_exists docker; then
+  lsb_dist=$( get_distribution )
+  lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+  echo "current system is $lsb_dist"
+  case "$lsb_dist" in
+    ubuntu|deepin|debian|raspbian|kylin)
+    	cp ../etc/docker.service /lib/systemd/system/docker.service
+    ;;
+  	centos|rhel|ol|sles|kylin|neokylin)
+			cp ../etc/docker.service /usr/lib/systemd/system/docker.service
+		;;
+    alios)
+      ip link add name docker0 type bridge
+      ip addr add dev docker0 172.17.0.1/16
+    	cp ../etc/docker.service /usr/lib/systemd/system/docker.service
+    ;;
+    *)
+			echo "unknown system to use /lib/systemd/system/docker.service"
+			cp ../etc/docker.service /lib/systemd/system/docker.service
+    ;;
+  esac
+
+  [ -d  /etc/docker/ ] || mkdir /etc/docker/  -p
+
+  chmod -R 755 ../cri
+  tar -zxvf ../cri/docker.tar.gz -C /usr/bin
+  chmod a+x /usr/bin
+  chmod a+x /usr/bin/docker
+  chmod a+x /usr/bin/dockerd
+  systemctl enable docker.service
+  systemctl restart docker.service
+  cp ../etc/daemon.json /etc/docker
+  if [[ -n $1 && -n $2 ]]; then
+    sed -i "s/$2:5000/$2:$3/g" /etc/docker/daemon.json
+  fi
+fi
+disable_selinux
+systemctl daemon-reload
+systemctl restart docker.service
---
+++ pkg/filesystem/rootfs/docker/scripts/init-registry.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Copyright © 2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -e
+set -x
+# prepare registry storage as directory
+cd $(dirname $0)
+
+REGISTRY_PORT=${1-5000}
+VOLUME=${2-/var/lib/registry}
+
+container=sealer-registry
+rootfs=$(dirname "$(pwd)")
+config="$rootfs/etc/registry_config.yml"
+htpasswd="$rootfs/etc/registry_htpasswd"
+certs_dir="$rootfs/certs"
+
+mkdir -p $VOLUME || true
+
+startRegistry() {
+    n=1
+    while (( $n <= 3 ))
+    do
+        echo "attempt to start registry"
+        (docker start $container && break) || (( n < 3))
+        (( n++ ))
+        sleep 3
+    done
+}
+
+[ -f ../images/registry.tar  ] && docker load -q -i ../images/registry.tar
+
+## rm container if exist.
+if [ "$(docker ps -aq -f name=$container)" ]; then
+    docker rm -f $container
+fi
+
+regArgs="-d --restart=always \
+--net=host \
+--name $container \
+-v $certs_dir:/certs \
+-v $VOLUME:/var/lib/registry \
+-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/sea.hub.crt \
+-e REGISTRY_HTTP_TLS_KEY=/certs/sea.hub.key"
+
+if [ -f $config ]; then
+    regArgs="$regArgs \
+    -v $config:/etc/docker/registry/config.yml"
+fi
+
+if [ -f $htpasswd ]; then
+    docker run $regArgs \
+            -v $htpasswd:/htpasswd \
+            -e REGISTRY_AUTH=htpasswd \
+            -e REGISTRY_AUTH_HTPASSWD_PATH=/htpasswd \
+            -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" registry:2.7.1 || startRegistry
+else
+    docker run $regArgs registry:2.7.1 || startRegistry
+fi
