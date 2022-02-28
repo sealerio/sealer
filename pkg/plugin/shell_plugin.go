af9a78e5ec92b3597aa2ac4ba86ed450a98a06e3
wangfei <wf01031309@alibaba-inc.com>
2022-02-28 14:08:46 +0800

    format

--- pkg/plugin/shell_plugin.go
+++ pkg/plugin/shell_plugin.go
@@ -65,7 +65,7 @@ func (s Sheller) Run(context Context, phase Phase) error {
 		} else if on == common.MASTER || on == common.NODE {
 			allHostIP = context.Cluster.GetIPSByRole(on)
 			if allHostIP == nil {
-				return fmt.Errorf("role is not found [%s]",on)
+				return fmt.Errorf("role is not found [%s]", on)
 			}
 		} else {
 			allHostIP = utils.DisassembleIPList(on)
