2fc6b30554b29181f8837d727308db5f76ddceea
wangfei <wf01031309@alibaba-inc.com>
2022-06-10 18:12:08 +0800

    faild

--- apply/processor/gen.go
+++ apply/processor/gen.go
@@ -18,6 +18,7 @@ package processor
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/sealerio/sealer/utils/yaml"
@@ -74,6 +75,12 @@ func NewGenerateProcessor() (Processor, error) {
 
 func (g *GenerateProcessor) init(cluster *v2.Cluster) error {
 	fileName := fmt.Sprintf("%s/.sealer/%s/Clusterfile", common.GetHomeDir(), cluster.Name)
+	clusterPath := fmt.Sprintf("%s/.sealer/%s", common.GetHomeDir(), cluster.Name)
+	err := os.MkdirAll(clusterPath, 0750)
+	if err != nil {
+		return err
+	}
+
 	if err := yaml.MarshalToFile(fileName, cluster); err != nil {
 		return err
 	}
