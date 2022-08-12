565ec36593869654628f8faeee14bef59527a829
wangfei <wf01031309@alibaba-inc.com>
2022-08-12 10:37:34 +0800

    Modify the returned error message syntax

--- pkg/container-runtime/dockerInstaller.go
+++ pkg/container-runtime/dockerInstaller.go
@@ -22,13 +22,13 @@ func (d DockerInstaller) InstallOn(hosts []net.IP) (*Info, error) {
 		IP := net.ParseIP(string(ip))
 		ssh, err := ssh.NewStdoutSSHClient(IP, d.cluster)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to new ssh client: %s", err)
 		}
 		registryConfig := registry.GetConfig(platform.DefaultMountClusterImageDir(d.cluster.Name), IP)
 		initCmd := fmt.Sprintf(RemoteChmod, rootfs, registryConfig.Domain, registryConfig.Port, d.info.conf.CgroupDriver, d.info.conf.LimitNofile)
 		err = ssh.CmdAsync(IP, initCmd)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to remote exec init cmd: %s", err)
 		}
 	}
 	return &d.info, nil
@@ -39,11 +39,11 @@ func (d DockerInstaller) UnInstallFrom(hosts []net.IP) error {
 		IP := net.ParseIP(string(ip))
 		client, err := ssh.NewStdoutSSHClient(IP, d.cluster)
 		if err != nil {
-			return fmt.Errorf("new ssh client failed: %s", err)
+			return fmt.Errorf("failed to new ssh client: %s", err)
 		}
 		err = client.CmdAsync(IP, CleanCmd)
 		if err != nil {
-			return fmt.Errorf("remote exec clean cmd failed: %s", err)
+			return fmt.Errorf("failed to remote exec clean cmd: %s", err)
 		}
 	}
 	return nil
