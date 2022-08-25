330c45cbab76bd23943a79d8d1f87775fb0307e1
kakzhou719 <wb-zl958011@alibaba-inc.com>
2022-08-25 14:00:34 +0800

    add hook

--- pkg/cluster-runtime/hook.go
+++ pkg/cluster-runtime/hook.go
@@ -1,14 +1,23 @@
-// alibaba-inc.com Inc.
-// Copyright (c) 2004-2022 All Rights Reserved.
+// Copyright © 2022 Alibaba Group Holding Ltd.
 //
-// @Author : huaiyou.cyz
-// @Time : 2022/8/7 10:48 PM
-// @File : hook
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package cluster_runtime
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 // HookConfig tell us how to configure hooks for cluster
 type HookConfig struct{}
@@ -42,6 +51,17 @@ func (i *Installer) AddHook(phase Phase, hook HookFunc) error {
 
 // run hooks
 func (i *Installer) runHook(phase Phase) error {
+	hookList, ok := i.hooks[phase]
+	if !ok {
+		return fmt.Errorf("failed to load hook: %s", phase)
+	}
+
+	for _, hook := range hookList {
+		if err := hook(); err != nil {
+			return fmt.Errorf("failed to run hook: %s", phase)
+		}
+	}
+
 	return nil
 }
 
