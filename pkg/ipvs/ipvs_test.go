680d45f4bd6d5b57e1f6695fe6a76ca9e1b36a27
huaiyou <huaiyou.cyz@alibaba-inc.com>
2022-10-24 15:44:52 +0800

    Add UT.
    Signed-off-by: huaiyou <huaiyou.cyz@alibaba-inc.com>

--- pkg/ipvs/ipvs_test.go
+++ pkg/ipvs/ipvs_test.go
@@ -14,39 +14,39 @@
 
 package ipvs
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
-/*var want = []string{
+var want = []string{
 	`apiVersion: v1
 kind: Pod
 metadata:
   creationTimestamp: null
-  labels:
-    component: kube-sealyun-lvscare
-    tier: control-plane
-  name: kube-sealyun-lvscare
+  name: kube-lvscare
   namespace: kube-system
 spec:
   containers:
   - args:
     - care
     - --vs
-    - 10.10.10.10:6443
+    - 10.107.2.1:6443
     - --health-path
     - /healthz
     - --health-schem
     - https
     - --rs
-    - 116.31.96.134:6443
+    - 172.16.228.157:6443
     - --rs
-    - 116.31.96.135:6443
+    - 172.16.228.158:6443
     - --rs
-    - 116.31.96.136:6443
+    - 172.16.228.159:6443
     command:
     - /usr/bin/lvscare
-    image: fanux/lvscare:latest
+    image: fdfadf
     imagePullPolicy: IfNotPresent
-    name: kube-sealyun-lvscare
+    name: kube-lvscare
     resources: {}
     securityContext:
       privileged: true
@@ -55,7 +55,44 @@ spec:
       name: lib-modules
       readOnly: true
   hostNetwork: true
-  priorityClassName: system-cluster-critical
+  volumes:
+  - hostPath:
+      path: /lib/modules
+      type: ""
+    name: lib-modules
+status: {}
+`,
+	`apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  name: kube-lvscare
+  namespace: kube-system
+spec:
+  containers:
+  - args:
+    - care
+    - --vs
+    - 10.107.2.1:6443
+    - --health-path
+    - /healthz
+    - --health-schem
+    - https
+    - --rs
+    - 172.16.228.157:6443
+    command:
+    - /usr/bin/lvscare
+    image: fdfadf
+    imagePullPolicy: IfNotPresent
+    name: kube-lvscare
+    resources: {}
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - mountPath: /lib/modules
+      name: lib-modules
+      readOnly: true
+  hostNetwork: true
   volumes:
   - hostPath:
       path: /lib/modules
@@ -67,39 +104,8 @@ status: {}
 
 func TestLvsStaticPodYaml(t *testing.T) {
 	type args struct {
-		vip     string
-		masters []string
-		image   string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			"test generate lvs care static pod",
-			args{
-				"10.10.10.10",
-				[]string{"116.31.96.134:6443", "116.31.96.135:6443", "116.31.96.136:6443"},
-				"fanux/lvscare:latest",
-			},
-			want[0],
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := LvsStaticPodYaml(tt.args.vip, tt.args.masters, tt.args.image); got != tt.want {
-				t.Errorf("LvsStaticPodYaml() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-*/
-
-func TestLvsStaticPodYaml(t *testing.T) {
-	type args struct {
-		vip     string
-		masters []string
+		vip     net.IP
+		masters []net.IP
 		image   string
 	}
 	tests := []struct {
@@ -109,22 +115,27 @@ func TestLvsStaticPodYaml(t *testing.T) {
 	}{
 		{
 			args: args{
-				vip:     "10.107.2.1",
-				masters: []string{"172.16.228.157", "172.16.228.157", "172.16.228.157"},
-				image:   "fdfadf",
+				vip: net.ParseIP("10.107.2.1"),
+				masters: []net.IP{
+					net.ParseIP("172.16.228.157"),
+					net.ParseIP("172.16.228.158"),
+					net.ParseIP("172.16.228.159")},
+				image: "fdfadf",
 			},
+			want: want[0],
 		},
 		{
 			args: args{
-				vip:     "10.107.2.1",
-				masters: []string{"172.16.228.157"},
+				vip:     net.ParseIP("10.107.2.1"),
+				masters: []net.IP{net.ParseIP("172.16.228.157")},
 				image:   "fdfadf",
 			},
+			want: want[1],
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := LvsStaticPodYaml(tt.args.vip, tt.args.masters, tt.args.image); got != tt.want {
+			if got, _ := LvsStaticPodYaml(tt.args.vip, tt.args.masters, tt.args.image); got != tt.want {
 				t.Errorf("LvsStaticPodYaml() = %v, want %v", got, tt.want)
 			}
 		})
