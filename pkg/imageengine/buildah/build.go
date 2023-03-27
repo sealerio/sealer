0f61a28adaca98ede0c68eb41deb862fb2274bf7
kakzhou719 <wb-zl958011@alibaba-inc.com>
2023-03-27 17:53:59 +0800

    Update containers vendor

--- pkg/imageengine/buildah/build.go
+++ pkg/imageengine/buildah/build.go
@@ -243,7 +243,7 @@ func (engine *Engine) wrapper2Options(opts *options.BuildOptions, wrapper *build
 
 	var excludes []string
 	if wrapper.IgnoreFile != "" {
-		if excludes, _, err = parse.ContainerIgnoreFile(contextDir, wrapper.IgnoreFile,nil); err != nil {
+		if excludes, _, err = parse.ContainerIgnoreFile(contextDir, wrapper.IgnoreFile, nil); err != nil {
 			return define.BuildOptions{}, []string{}, err
 		}
 	}
